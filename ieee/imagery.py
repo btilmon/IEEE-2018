e750873be91f0318f0069f7cab9af390662775c1
btilmon <brevinjt@gmail.com>
2019-03-31 23:53:56 +0100

    both cameras functional

--- ieee/imagery.py
+++ ieee/imagery.py
@@ -1,102 +1,20 @@
-#~ from picamera.array import PiRGBArray
-#~ from picamera import PiCamera
-#~ import time
 
-#~ camera = PiCamera()
-#~ rawCapture = PiRGBArray(camera)
-
-#~ time.sleep(0.1)
-
-#~ camera.capture(rawCapture, format = "bgr")
-#~ image = rawCapture.array
-
-#~ print(image.shape)
-
-#~ import picamera 
-#~ import os
-#~ from PIL import Image
-#~ from time import sleep
-#~ import numpy as np
-
-#~ camera = picamera.PiCamera()
-#~ camera.start_preview()
-#~ camera.capture('/home/pi/Desktop/image.jpg')
-#~ sleep(30)
-#~ camera.stop_preview()
-
-#~ img = np.asarray(Image.open('/home/pi/Desktop/image.jpg').convert('L'))
-#~ img = Image.open('/home/pi/Desktop/image.jpg').convert('L')
-#~ img.show()
-#~ print(len(img))
-#~ img = np.asarray([img])
-#~ img.save('/home/pi/Desktop/grey.jpg')
-
-
-
-#~ os.remove("/home/pi/Desktop/image.jpg")
-
-
-
-# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
 import numpy as np
+import sys
+#~ import skvideo.io
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-# allow the camera to warmup
-time.sleep(0.1)
-
-#~ boundaries = [
-	#~ ([17, 15, 100], [80, 80, 200]),
-	#~ ([86, 31, 4], [220, 88, 50]),
-	#~ ([25, 146, 190], [62, 174, 250]),
-	#~ ([103, 86, 65], [145, 133, 128])
-#~ ]
-
-#~ # capture frames from the camera
-#~ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    #~ # grab the raw NumPy array representing the image, then initialize the timestamp
-    #~ # and occupied/unoccupied text
-    #~ image = frame.array
-    #~ for (lower, upper) in boundaries:
-		
-		#~ lower = np.array(lower, dtype = "uint8")
-		#~ upper = np.array(upper, dtype = "uint8")
-		
-		#~ mask = cv2.inRange(image, lower, upper)
-		#~ output = cv2.bitwise_and(image, image, mask = mask)
-		
-		#~ cv2.imshow("images", np.hstack([image, output]))
-		#~ cv2.waitKey(0)
-		
-    # show the frame
-    #~ cv2.imshow("Frame", image)
-    #~ key = cv2.waitKey(1) & 0xFF
-    
-    
-    # clear the stream in preparation for the next frame
-    #~ rawCapture.truncate(0)
-
-    #~ # if the `q` key was pressed, break from the loop
-    #~ if key == ord("q"):
-        #~ break
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-    
+def segmentedImage(image):
+    '''
+    - Returns segmented image with a red, blue, green, and yellow mask.
+    '''
     #converting from bgr to hsv color scale
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
+    #red thresholds
     #range for lower red
     lower_red = np.array([0,120,70])
     upper_red = np.array([10,255,255])
@@ -130,22 +48,73 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     bmask = cv2.inRange(hsv, lower_blue, upper_blue)
     bmask = cv2.morphologyEx(bmask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
     blue = cv2.morphologyEx(bmask, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))    
-        
-        
     
+    #add masks to segment r,g,b,y
     mask = green + red + yellow + blue
     final = cv2.bitwise_and(image, image, mask = mask)
     
+    return final
 
-    
-    #~ # show the frame
-    cv2.imshow("Frame", final)
-    key = cv2.waitKey(1) & 0xFF
-    
-    
-    #~ # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 15
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
+# allow the camera to warmup
+time.sleep(0.1)
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, image1 = cap.read()
+
+
+   
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image2 = frame.array
+        rawCapture.truncate(0)
+        break
+        
+    objects = segmentedImage(image1)
+    tape = segmentedImage(image2)
+
+    horizontal = np.hstack((objects,tape))
+
+    cv2.imshow('frame',horizontal)
+    #~ cv2.imshow('frame',tape)    
+    
+    #~ rawCapture.truncate(0)
+    #~ cap.release()
+    
     # if the `q` key was pressed, break from the loop
+    key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
+        
+#main pipeline
+#~ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+    #~ #capture pi camera image
+    #~ image1 = frame.array 
+    #~ #capture usb camera image
+
+    #~ cv2.imshow('frame',image2)
+    #~ cv2.imshow('frame',image1)    
+    
+    #~ rawCapture.truncate(0)
+    #~ cap.release()
+    
+    #~ # if the `q` key was pressed, break from the loop
+    #~ key = cv2.waitKey(1) & 0xFF
+    #~ if key == ord("q"):
+        #~ break
+
+
+
+    #return segmented images
+    #~ objects = segmentedImage(image1) 
+    #~ tape = segmentedImage(image2) 
+    
+    #~ print(objects)
+    #Delete pi cam and usb cam images for memory
+
