4c10ad29af9f30562b904b776b60325dee423d88
btilmon <brevinjt@gmail.com>
2019-04-02 04:43:48 +0100

    cv_pipeline is main code for vision stuff

--- ieee/camera_object.py
+++ ieee/camera_object.py
@@ -6,13 +6,38 @@ import argparse
 import imutils
 import time
 import cv2
+import numpy as np
+from sklearn.cluster import KMeans
  
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--picamera", type=int, default=-1,
+ap.add_argument("-p", "--picamera", type=int, default=1,
 	help="whether or not the Raspberry Pi camera should be used")
 args = vars(ap.parse_args())
  
 # initialize the video stream and allow the cammera sensor to warmup
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(2.0)
+
+
+
+
+#~ def objectColor():
+# loop over the frames from the video stream
+def objectImage():
+    
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        #~ print(frame)
+        #~ # show the frame
+
+        return frame
+
+    cv2.destroyAllWindows()
+    vs.stop()
+
+#~ while True:
+    #~ objectImage()
